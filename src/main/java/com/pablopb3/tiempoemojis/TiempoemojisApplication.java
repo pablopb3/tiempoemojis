68f6118f1ad67595fd596d71d2cb040cfe74d539
Pablo Perez <pablopb3@gmail.com>
2020-12-08 13:26:59 +0100

    project init

---
+++ src/main/java/com/pablopb3/tiempoemojis/TiempoemojisApplication.java
@@ -0,0 +1,13 @@
+package com.pablopb3.tiempoemojis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiempoemojisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiempoemojisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablopb3/tiempoemojis/TiempoemojisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablopb3.tiempoemojis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiempoemojisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
